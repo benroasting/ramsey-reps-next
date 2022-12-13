cfc7d416505f4dc3f47404003a7cf96fabe8677b
Ben Young <ben.young@ramseysolutions.com>
2022-12-12 20:45:06 -0600

    added basic structure for registration and login pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 export default function login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Layout title="Create Account">
+        <form onSubmit={handler}>
+          <div className="mb-4">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" autoFocus />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" autoFocus />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </Layout>
+    </div>
+  );
 }
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import Layout from "../components/Layout";
+import handler from "./api/hello";
 
 export default function register() {
-  return <div></div>;
+  return (
+    <div>
+      <Layout title="Create Account">
+        <form onSubmit={handler}>
+          <div className="mb-4">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" autoFocus />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" autoFocus />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="name" autoFocus />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="teamName">Team Name</label>
+            <input type="text" id="teamName" autoFocus />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" autoFocus />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" autoFocus />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </Layout>
+    </div>
+  );
 }
